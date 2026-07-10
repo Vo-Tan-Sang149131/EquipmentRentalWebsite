020d119a8d763eaa0530866b6c3c45e62b302157
Vo Tan Sang <votansang1711@gmail.com>
2026-07-11 03:41:53 +0700

    chore: design new configs for dev and prod environment backend

--- apps/backend/src/main/java/com/example/demo/controller/chat/ChatController.java
+++ apps/backend/src/main/java/com/example/demo/controller/chat/ChatController.java
@@ -24,7 +24,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chat")
-@Validated
 public class ChatController extends BaseController {
 
     private final ChatService chatService;
